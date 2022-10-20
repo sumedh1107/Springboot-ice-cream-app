c4465baa479b0c442c9463b9b2e4fd9329eeecf3
sumedh1107 <sumeghd@gmail.com>
2022-10-20 10:07:11 +0530

    initial commit

---
+++ src/main/java/com/sumedh/java/springboot/icecream/SpringbootIceCreamAppApplication.java
@@ -0,0 +1,13 @@
+package com.sumedh.java.springboot.icecream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootIceCreamAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootIceCreamAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sumedh/java/springboot/icecream/SpringbootIceCreamAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumedh.java.springboot.icecream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootIceCreamAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
